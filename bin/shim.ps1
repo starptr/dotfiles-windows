5ad4c967290258e3dbba37ed81d327e7b0645471
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-19 23:45:27 -0700

    Move powershell profile to home

--- bin/shim.ps1
+++ bin/shim.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 
 # Hardlink powershell profile
-cmd.exe /c "mklink $([Environment]::GetFolderPath("MyDocuments"))\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+cmd.exe /c "mklink $profile $HOME\profile.ps1"
