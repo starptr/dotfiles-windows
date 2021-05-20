80d8a287450b2f9edf70a1a026ecf99ab93de222
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-19 23:22:58 -0700

    Fix symlink (no admin)

--- bin/shim.ps1
+++ bin/shim.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 
 # Hardlink powershell profile
-New-Item -Path "$([Environment]::GetFolderPath("MyDocuments"))\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -ItemType HardLink -Value "~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Force
+cmd.exe /c "mklink $([Environment]::GetFolderPath("MyDocuments"))\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
