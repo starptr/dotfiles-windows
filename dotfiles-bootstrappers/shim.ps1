a6e8563a1ec43a297daf4a8739f375f247ba1775
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-21 15:21:58 -0700

    Fix alacritty config to use junction instead

--- dotfiles-bootstrappers/shim.ps1
+++ dotfiles-bootstrappers/shim.ps1
@@ -4,4 +4,4 @@ $ErrorActionPreference = "Stop"
 cmd.exe /c "mklink $profile $HOME\profile.ps1"
 
 # Link alacritty config
-cmd.exe /c "mklink $env:appdata\alacritty\alacritty.yml $HOME\dotfiles-linux\.config\alacritty\alacritty.yml"
+cmd.exe /c "mklink /j $env:appdata\alacritty $HOME\dotfiles-linux\.config\alacritty"
