0baa67d6de2f0f96e6bb0bafd98350c35791e062
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-20 23:56:41 -0700

    Bootstrap alacritty shortcut

--- dotfiles-bootstrappers/shim.ps1
+++ dotfiles-bootstrappers/shim.ps1
@@ -1,4 +1,10 @@
 $ErrorActionPreference = "Stop"
 
-# Hardlink powershell profile
+# Link powershell profile
 cmd.exe /c "mklink $profile $HOME\profile.ps1"
+# Create alacritty shortcut
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$HOME\Desktop\a2.lnk")
+$Shortcut.TargetPath = "$(which alacritty)"
+$Shortcut.Arguments = "--working-directory $HOME --config-file $HOME\dotfiles-linux\.config\alacritty\alacritty.yml"
+$Shortcut.Save()
