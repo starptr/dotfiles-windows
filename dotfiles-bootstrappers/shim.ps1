6733d8652b290b62558d15230018083e3a61b649
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-21 15:16:43 -0700

    Change alacritty config to symlink

--- dotfiles-bootstrappers/shim.ps1
+++ dotfiles-bootstrappers/shim.ps1
@@ -2,9 +2,6 @@ $ErrorActionPreference = "Stop"
 
 # Link powershell profile
 cmd.exe /c "mklink $profile $HOME\profile.ps1"
-# Create alacritty shortcut
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$HOME\Desktop\a2.lnk")
-$Shortcut.TargetPath = "$(which alacritty)"
-$Shortcut.Arguments = "--working-directory $HOME --config-file $HOME\dotfiles-linux\.config\alacritty\alacritty.yml"
-$Shortcut.Save()
+
+# Link alacritty config
+cmd.exe /c "mklink $env:appdata\alacritty\alacritty.yml $HOME\dotfiles-linux\.config\alacritty\alacritty.yml"
