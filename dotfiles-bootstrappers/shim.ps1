329d8eeb5252bacb23e1f5c495f7498fbb9f22e4
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-21 20:23:09 -0700

    Add wezterm config junction

--- dotfiles-bootstrappers/shim.ps1
+++ dotfiles-bootstrappers/shim.ps1
@@ -3,5 +3,8 @@ $ErrorActionPreference = "Stop"
 # Link powershell profile
 cmd.exe /c "mklink $profile $HOME\profile.ps1"
 
-# Link alacritty config
+# Junction to alacritty config
 cmd.exe /c "mklink /j $env:appdata\alacritty $HOME\dotfiles-linux\.config\alacritty"
+
+# Junction to wezterm config
+cmd.exe /c "mklink /j $HOME\.config\wezterm $HOME\dotfiles-linux\.config\wezterm"
