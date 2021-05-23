063abc8bf0b42e9d64c33d18447cb61b51b720aa
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-22 18:35:43 -0700

    Add aliases

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,14 @@
-New-Alias l ls
+# Set common env vars
 $env:EDITOR = "nvim"
 $env:VISUAL = "code"
 
+# Add essentials dirs to path
 $env:Path = "$HOME\bin;$env:Path"
+
+# Set aliases
+function l { ls.exe --color -A }
+function la { ls.exe --color -al }
+function nve { neovide.exe }
 
 # Initialize starship
 #$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
