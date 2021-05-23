06e7adb6ee93093e6ca3947a0bfe1c227b2a883c
Yuto Nishida <yuyu.nishida@gmail.com>
2021-05-22 22:05:17 -0700

    Config lsd and chezmoi aliases

--- profile.ps1
+++ profile.ps1
@@ -8,6 +8,26 @@ $env:Path = "$HOME\bin;$env:Path"
 # Set aliases
 function l { ls.exe --color -A }
 function la { ls.exe --color -al }
+function d { lsd }
+function da { lsd -al }
+
+function cm { chezmoi }
+function cmr { chezmoi -vn apply }
+function cmw { chezmoi -v apply }
+function cme {
+	param (
+		$filename
+		  )
+	chezmoi edit $filename
+}
+function cma {
+	param (
+		$filename
+		  )
+	chezmoi add $filename
+}
+function cmg { chezmoi cd }
+
 function nve { neovide.exe }
 
 # Enable tab completion
