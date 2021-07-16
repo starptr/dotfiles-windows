4e16410416d4b67297b2e1c57829c766c5d42317
Yuto Nishida <yuyu.nishida@gmail.com>
2021-07-15 20:04:03 -0700

    Config add cc_path function

--- profile.ps1
+++ profile.ps1
@@ -18,10 +18,20 @@ $env:Path = "$wezterm_dir_computed;$env:Path"
 #$env:Path = "$vcpp_build_tools_dir_computed;$env:Path"
 
 # Set rustup env vars
-$env:CARGO_HOME = "$env:SCOOP\persist\rustup-msvc\.cargo"
-$env:RUSTUP_HOME = "$env:SCOOP\persist\rustup-msvc\.rustup"
+#$env:CARGO_HOME = "$env:SCOOP\persist\rustup-msvc\.cargo"
+#$env:RUSTUP_HOME = "$env:SCOOP\persist\rustup-msvc\.rustup"
 # Add rust bin dir
 $env:Path = "$env:SCOOP\persist\rustup-msvc\.cargo\bin;$env:Path"
+
+# Fix neovim pyenv root detection
+$env:PYENV_ROOT = "$env:SCOOP\apps\pyenv\current"
+
+# Add cc to path
+# Requires msys2 with pkgs
+function cc_path {
+	$env:Path = "$env:SCOOP\apps\msys2\current\mingw64\bin;$env:Path"
+}
+# Just call cargo from msys2!
 
 # Set aliases
 function l { ls.exe --color -A }
